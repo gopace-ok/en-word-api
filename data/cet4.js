69809f6d71b1b7ab828af47b3d3bc852ba66cf19
gopace-ok <154227343+gopace-ok@users.noreply.github.com>
2025-04-14 11:59:11 -0400

    Create cet4.js

---
+++ data/cet4.js
@@ -0,0 +1,13 @@
+module.exports = [
+  { word: "abandon", translation: "放弃，抛弃" },
+  { word: "ability", translation: "能力，才能" },
+  { word: "abroad", translation: "国外，海外" },
+  { word: "absence", translation: "缺席，不在场" },
+  { word: "absolute", translation: "绝对的，完全的" },
+  { word: "absorb", translation: "吸收，吸引" },
+  { word: "abstract", translation: "抽象的，理论的" },
+  { word: "academic", translation: "学术的，教学的" },
+  { word: "accept", translation: "接受，同意" },
+  { word: "access", translation: "通道，接近" }
+  // 可以继续添加更多单词
+];
