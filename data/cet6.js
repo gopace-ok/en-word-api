6b208e96b47b1887851d08d951889a7993c64e63
gopace-ok <154227343+gopace-ok@users.noreply.github.com>
2025-04-14 11:59:27 -0400

    Create cet6.js

---
+++ data/cet6.js
@@ -0,0 +1,13 @@
+module.exports = [
+  { word: "abolish", translation: "废除，取消" },
+  { word: "abrupt", translation: "突然的，意外的" },
+  { word: "absurd", translation: "荒谬的，荒唐的" },
+  { word: "abundant", translation: "丰富的，充裕的" },
+  { word: "accessory", translation: "附件，配件" },
+  { word: "accommodate", translation: "容纳，使适应" },
+  { word: "accomplishment", translation: "成就，完成" },
+  { word: "accountability", translation: "责任，义务" },
+  { word: "accumulate", translation: "积累，积聚" },
+  { word: "accuracy", translation: "准确性，精确度" }
+  // 
+];
